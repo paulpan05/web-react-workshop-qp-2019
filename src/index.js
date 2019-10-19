4b4b6fa79abc2adf494befc21dd9c38538fd7cb5
Paul Pan <panjunhong05@gmail.com>
2019-10-19 13:13:39 -0700

    Workshop test

--- src/index.js
+++ src/index.js
@@ -4,13 +4,27 @@ import ReactDOM from "react-dom";
 import "./styles.css";
 
 const App = () => {
+  const date = new Date();
+  const initialTime = date.getTime();
+  const [time, setTime] = React.useState(initialTime);
+  const [timerStyle, setTimerStyle] = React.useState("h1-entering");
+  setTimeout(() => setTime(date.getTime()), 1000);
+  const handleAnimateIn = () => {
+    let element = document.querySelector("#timer");
+    if (element) {
+      setTimerStyle("h1-entered");
+    }
+  };
+  React.useEffect(handleAnimateIn, []);
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
+      <h1 id="timer" className={timerStyle}>
+        Hello CodeSandbox {time}
+      </h1>
       <h2>Start editing to see some magic happen!</h2>
     </div>
   );
-}
+};
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
